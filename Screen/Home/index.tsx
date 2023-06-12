d2a81e205af6bc4f6c4f99010efc021e7b383030
Prabhash Rai <prabhashsahaj@gmail.com>
2023-06-12 12:14:13 +0530

    adds static chart view

--- Screen/Home/index.tsx
+++ Screen/Home/index.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import StaticChart from "../StaticChart";
+
 const Home = () => {
     return (
-        <> HomePage </>
+        <>
+            <StaticChart />
+        </>
     )
 }
 
