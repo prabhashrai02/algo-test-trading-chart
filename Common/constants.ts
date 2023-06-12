c2da65352b6b3f0e6916a599384d8a5f54e12bcd
Prabhash Rai <prabhashsahaj@gmail.com>
2023-06-12 23:01:04 +0530

    move common constants to Common

---
+++ Common/constants.ts
@@ -0,0 +1,8 @@
+export const STOCKS_LIST = [
+    "BANKNIFTY2360843500CE(2023-06-01)",
+    "BANKNIFTY2360843500PE(2023-06-01)",
+    "BANKNIFTY2361543500CE(2023-06-01)",
+  ];
+  
+  export const TIME_FRAME = ['1 Min', '3 Min', '5 Min', '10 Min', '15 Min', '30 Min'];
+  
