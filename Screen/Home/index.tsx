61f369e2319200197127e181654d7fbd63e2071a
Prabhash Rai <prabhashsahaj@gmail.com>
2023-06-12 16:38:58 +0530

    separated function to custom hooks

--- Screen/Home/index.tsx
+++ Screen/Home/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import MultipleInstruments from "../MultipleInstruments";
 import OHLCPage from "../OHLC";
 import StaticChart from "../StaticChart";
 
 const Home = () => {
     return (
         <>
+            <MultipleInstruments />
             <OHLCPage />
             <StaticChart />
         </>
